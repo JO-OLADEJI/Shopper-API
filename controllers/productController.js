0698afc3ba139872e6ccef09c05208fa10b84aea
Joshua Oladeji <dejijolaoluwa@gmail.com>
2021-06-29 00:59:52 +0100

    created logic to read all products from db

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,19 +4,32 @@ const { Product } = require('../models/product.js');
 class ProductController {
 
   allProducts = async (req, res) => {
-
+    // get all products from the db
+    try {
+      const productsList = await Product.find();
+      res.json({ 'success': true, 'result': productsList });
+    }
+    catch(err) {
+      res.json({ 'success': false });
+    }
   }
 
   createProduct = async (req, res) => {
+    // validate the product for errors
 
+    // create a new product from the model
   }
 
   updateProduct = async (req, res) => {
+    // validate the product
 
+    // update 'date_edited' field of the product
+
+    // update the product by id
   }
 
   deleteProduct = async (req, res) => {
-
+    // delete a product by id
   }
 
 }
